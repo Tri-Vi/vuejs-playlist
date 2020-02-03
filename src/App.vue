3fc1199d6ea2c4a51ef1dcb5ba96294b3935622b
Tri-Vi <36706183+Tri-Vi@users.noreply.github.com>
2020-02-03 16:32:05 -0600

    EOD - 02/03/20
    @tri-vi
    
    - Create a simple blog using input-binding

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,27 @@
 <template>
   <div>
-    <app-header v-bind:title="title" v-on:changeTitle="udpateTitle($event)"></app-header>
-    <app-ninjas v-bind:ninjas="ninjas"></app-ninjas>
-    <button @click="deleteNinja">Delete Ninja</button>
-    <app-footer v-bind:title="title"></app-footer>
+   <add-blog></add-blog>
   </div>
 </template>
 
 <script>
-import Header from './components/Header.vue'
-import Footer from './components/Footer.vue'
-import Ninjas from './components/Ninjas.vue'
+import addBlog from './components/addBlog.vue';
 
 export default {
   components: {
-    'app-header': Header,
-    'app-footer': Footer,
-    'app-ninjas': Ninjas
+    'add-blog': addBlog
   },
   data () {
     return {
-      ninjas: [
-        {id: 1, name: 'Ryu', speciality: 'Vue Components', show: false},
-        {id: 2, name: 'Crystal', speciality: 'HTML Wizardy', show: false},
-        {id: 3, name: 'Hitoshi', speciality: 'Click Events', show: false},
-        {id: 4, name: 'Tango', speciality: 'Conditionals', show: false},
-        {id: 5, name: 'Kami', speciality: 'Webpack', show: false},
-        {id: 6, name: 'Yoshi', speciality: 'Data Design', show: false}
-      ],
-      title: "Vue Title"
+      
     }
   },
   methods: {
-    udpateTitle(e){
-      this.title = e;
-    },
-    deleteNinja(){
-      this.ninjas.pop();
-    }
+    
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 
-h1, h2 {
-  font-weight: normal;
-}
 </style>
