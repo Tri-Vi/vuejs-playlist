8aa60035cbdc231f1718a71abae875c042fad719
Tri-Vi <36706183+Tri-Vi@users.noreply.github.com>
2020-02-04 15:00:23 -0600

    MOD - 02/04/20 - 01
    @tri-vi
    
    - Implement select box binding

--- src/components/addBlog.vue
+++ src/components/addBlog.vue
@@ -16,6 +16,12 @@
         <label for="">Cheese</label>
         <input type="checkbox" value="cheese" v-model="blog.categories"/>
       </div>
+
+      <label>Author</label>
+      <select v-model="blog.author">
+        <option value=""></option>
+        <option v-for="author in authors">{{author}}</option>
+      </select>
     </form>
 
     <div id="preview">
@@ -27,6 +33,7 @@
       <ul>
         <li v-for="category in blog.categories">{{category}}</li>
       </ul>
+      <p>Author: {{blog.author}}</p>
     </div>
   </div>
 </template>
@@ -41,8 +48,10 @@ export default {
       blog: {
         title: "",
         content: "",
-        categories: []
-      }
+        categories: [],
+        author: ""
+      },
+      authors: ['The Net Ninja', 'The Angular Avenger', 'The Vue Vinidicator']
     }
   },
   methods: {
