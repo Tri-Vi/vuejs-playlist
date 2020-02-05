0f38e1ac3e5ab94cbce4c432866ac2dac5b459d5
Tri-Vi <36706183+Tri-Vi@users.noreply.github.com>
2020-02-05 11:40:21 -0600

    MOD - 02/05/20
    @tri-vi
    
    - Implement GET request

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,21 @@
 <template>
   <div>
+  
+    <show-blog></show-blog>
+    <!--
    <add-blog></add-blog>
+    -->
   </div>
 </template>
 
 <script>
 import addBlog from './components/addBlog.vue';
+import showBlog from './components/showBlog.vue';
 
 export default {
   components: {
-    'add-blog': addBlog
+    'add-blog': addBlog,
+    'show-blog': showBlog
   },
   data () {
     return {
