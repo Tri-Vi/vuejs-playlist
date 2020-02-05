82d2d700a5e93a197fa06f656a0f604750bcd59e
Tri-Vi <36706183+Tri-Vi@users.noreply.github.com>
2020-02-05 15:02:29 -0600

    MOD - 02/05/20 - 04
    @tri-vi
    
    - Register things (filters / directives) locally

--- src/components/showBlog.vue
+++ src/components/showBlog.vue
@@ -36,6 +36,21 @@ export default {
         return blog.title.match(this.search);
       });
     }
+  },
+  filters: {
+    toUppercase(value){
+      return value.toUpperCase();
+    },
+    snippet(value){
+      return value.slice(0, 100) + '...';
+    }
+  },
+  directives: {
+    'rainbow': {
+      bind(el, binding, vnode){
+        el.style.color =  "#" + Math.random().toString().slice(2,8);
+      }
+    }
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -4,12 +4,12 @@ import VueResource from 'vue-resource'
 
 Vue.use(VueResource);
 
-// Custom directive
-Vue.directive('rainbow', {
-  bind(el, binding, vnode){
-    el.style.color =  "#" + Math.random().toString().slice(2,8);
-  }
-});
+// Custom directive - Globally
+// Vue.directive('rainbow', {
+//   bind(el, binding, vnode){
+//     el.style.color =  "#" + Math.random().toString().slice(2,8);
+//   }
+// });
 
 Vue.directive('theme', {
   bind(el, binding, vnode){
@@ -26,14 +26,14 @@ Vue.directive('theme', {
   }
 });
 
-// Filters
-Vue.filter('to-uppercase', function(value){
-  return value.toUpperCase();
-});
+// Filters - Globally
+// Vue.filter('to-uppercase', function(value){
+//   return value.toUpperCase();
+// });
 
-Vue.filter('snippet', function(value){
-  return value.slice(0, 100) + '...';
-});
+// Vue.filter('snippet', function(value){
+//   return value.slice(0, 100) + '...';
+// });
 
 new Vue({
   el: '#app',
